534bccbb5093d67a81e744d3c5cbeab59cc2877a
Faris <alarifinmirzafaris@gmail.com>
2025-07-01 21:33:06 +0700

    feat: implement dynamic grid creation with user input

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 main()
 
 function main() {
-    let numberOfGrid = 16;
     const container = document.querySelector(".container");
-    createGrids(container, numberOfGrid);
+    const inputButton = document.querySelector("#input-button");
+    inputButton.addEventListener("click", () => {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        let numberOfGrid = 0;
+        do {
+            numberOfGrid = Number(window.prompt("Enter the number of grids", 16));
+        } while (numberOfGrid > 100);
+        
+        createGrids(container, numberOfGrid);
+    })
 }
 
 function createGrids(parent, n) {
@@ -21,11 +31,11 @@ function createSingleGrid(parent, n) {
         const grid = document.createElement("div");
         grid.style.border = "1px solid #000000";
         grid.style.flex = "1";
-        
+
         grid.addEventListener("mouseover", () => {
             grid.style.backgroundColor = "#000000";
         })
-        
+
         parent.appendChild(grid);
     }
 }
