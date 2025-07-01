0f46e209887a7d28d22831c2f5a7e50860cc9fa2
Faris <alarifinmirzafaris@gmail.com>
2025-07-01 17:20:38 +0700

    feat: make grid "painted" when mouse hovering

--- scripts.js
+++ scripts.js
@@ -11,19 +11,27 @@
  */
 
 const container = document.querySelector(".container");
-createGrid(16);
+createGrids(16);
 
-function createGrid(numberOfGrid) {
+function createGrids(numberOfGrid) {
     for (let i = 0; i < numberOfGrid; i++) {
         const gridContainer = document.createElement("div");
         gridContainer.style.display = "flex";
         container.appendChild(gridContainer);
         for (let j = 0; j < numberOfGrid; j++) {
             const grid = document.createElement("div");
-            grid.style.width = "60px";
-            grid.style.height = "60px";
-            grid.style.border = "1px solid black";
+            createGrid(grid);
             gridContainer.appendChild(grid);
         }
     }
+}
+
+function createGrid(grid) {
+    grid.style.minWidth = "60px";
+    grid.style.minHeight = "60px";
+    grid.style.border = "1px solid black";
+    grid.style.flexShrink = "1";
+    grid.addEventListener("mouseover", () => {
+        grid.classList.add("paint")
+    })
 }
